ac6f00c5b624a5ba5cdce8947fcba9354768d733
AnassAzeroual <35193529+AnassAzeroual@users.noreply.github.com>
2023-12-01 23:23:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# test-interview
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/AnassAzeroual/test-interview)
---
+++ index.js
@@ -0,0 +1,41 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function main(params) {
+  const splitedParams = params.split('');
+  const splitedHash = splitedParams.map((v) => {
+    if (v === '(') return 'p-o';
+    else if (v === ')') return 'p-c';
+    else if (v === '{') return 'b-o';
+    else if (v === '}') return 'b-c';
+    else if (v === '[') return 's-o';
+    else if (v === ']') return 's-c';
+  });
+  let level = [];
+  for (const item of splitedHash) {
+    if (level.length === 0) level.push(item);
+    if (
+      level.at(-1) &&
+      level.at(-1).split('-')[0] === item.split('-')[0] &&
+      level.at(-1).split('-')[1] === 'o' &&
+      item.split('-')[1] === 'c'
+    ) {
+      level.pop();
+    }
+    // console.log(level);
+  }
+  if (level.length === 0) {
+    console.log(params + ' is balnced');
+  } else {
+    console.log(params + ' is not balnced');
+  }
+}
+
+main('{()}[]');
+main('{(})[]');
+main('{()[]}');
+main('{})([]');
